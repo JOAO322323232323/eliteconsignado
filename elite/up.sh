4d13c50ad1e94c5445d75dd086b1bb9c1bc1b5af
VICTOR <68675355+JOAO322323232323@users.noreply.github.com>
2023-10-18 13:08:30 -0300

    Update up.sh

--- elite/up.sh
+++ elite/up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf elite/
-git clone git@github.com:JOAO322323232323/eliteconsignado.git
+rm -rf eliteconsignado/
+git clone https://github.com/JOAO322323232323/eliteconsignado.git
 cd elite/
 sudo docker-compose build
 sudo docker-compose up -d
